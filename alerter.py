6e78aa89170476de15a65bec7bc2bd131d327f68
Fynn Ruehlemann <fynnruehlemann@gmail.com>
2024-12-10 16:40:09 +0100

    selenium integration

--- alerter.py
+++ alerter.py
@@ -1,16 +1,20 @@
 import time
 import requests
-import subprocess
+from selenium.webdriver import Edge
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.edge.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
 from bs4 import BeautifulSoup
 
-URL = "https://twitch.facepunch.com/"
-
 box_status = {}
-done = {0, 1, 4}
+done = {0, 1, 4, 8}
 priority_list = [8, 9, 2, 5, 7, 6, 10, 11, 3]  # Prio Liste
-view_time = {0: 100, 1: 100, 2: 0, 3: 0, 4: 100, 5: 0, 6: 0, 7: 0, 8: 100, 9: 99.9, 10: 0, 11: 0}  # Prozentualer Fortschritt der Drops
+view_time = {0: 100, 1: 100, 2: 0, 3: 0, 4: 100, 5: 0, 6: 0, 7: 0, 8: 100, 9: 99.8, 10: 0, 11: 0}  # Prozentualer Fortschritt der Drops
 current_stream = None
-current_window = None
+driver = None
 stream_start_time = {}
 WATCH_INTERVAL = 0.2
 DROP_DURATION = 120
@@ -27,24 +31,49 @@ def percent_to_minutes(percent):
 def minutes_to_percent(minutes):
     return (minutes / DROP_DURATION) * 100
 
-def close_stream():
-    if current_window:
-        subprocess.check_call(['taskkill', '/F', '/T', '/PID', str(current_window.pid)])
+def login_to_twitch():
+    global driver
+
+    service = Service("./geckodriver.exe")
+
+    options = webdriver.FirefoxOptions()
+    options.set_preference("media.volume_scale", "0.0")
+
+    if not driver:
+        driver = Edge(options=options, service=service)
+
+    driver.get("https://www.twitch.tv/login")
+
+    time.sleep(2)
+
+    username_field = driver.find_element(By.ID, "login-username")
+    password_field = driver.find_element(By.ID, "password-input")
+
+    with open("login-data.txt", "r") as file:
+        lines = file.readlines()
+        username = lines[0].strip()
+        password = lines[1].strip()
+
+    username_field.send_keys(username)
+    password_field.send_keys(password)
+
+    password_field.send_keys(Keys.RETURN)
+
+    time.sleep(5)
+
+    return driver
 
 def open_stream(url, stream_index):
-    global current_window, stream_start_time
+    global driver, stream_start_time
 
-    close_stream()
-
-    edge_path = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
-    current_window = subprocess.Popen([edge_path, url])
+    driver.get(url)
 
     stream_start_time[stream_index] = time.time()
 
 def check_online_status():
     global box_status, current_stream, view_time
 
-    response = requests.get(URL)
+    response = requests.get("https://twitch.facepunch.com/")
     soup = BeautifulSoup(response.text, "html.parser")
     boxes = soup.find_all("div", class_="drop-box")
     best_priority = float('inf')
@@ -73,7 +102,6 @@ def check_online_status():
             current_stream = best_drop_index
     elif current_stream is not None and view_time[current_stream] >= 100:
         print(f"Drop {current_stream} abgeschlossen. Schließe Stream.")
-        close_stream()
 
         current_stream = None
 
@@ -100,6 +128,7 @@ def update_view_time():
 
 def main():
     print("Monitoring gestartet... Drücken Sie STRG+C zum Beenden.")
+    login_to_twitch()
     try:
         while True:
             print(time.strftime("%H:%M", time.localtime()))
@@ -108,7 +137,6 @@ def main():
             print()
             time.sleep(WATCH_INTERVAL * 60)
     except KeyboardInterrupt:
-        close_stream()
         print("Monitoring beendet.")
 
 if __name__ == "__main__":
